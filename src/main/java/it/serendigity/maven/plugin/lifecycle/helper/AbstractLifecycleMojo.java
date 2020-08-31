e99daec475656a187b180e191a6a48b6121fd653
SerendipityOfficialDev <68677615+SerendipityOfficialDev@users.noreply.github.com>
2020-08-31 16:16:03 +0200

    Fix deprecated API StringUtils

--- src/main/java/it/serendigity/maven/plugin/lifecycle/helper/AbstractLifecycleMojo.java
+++ src/main/java/it/serendigity/maven/plugin/lifecycle/helper/AbstractLifecycleMojo.java
@@ -4,6 +4,7 @@ package it.serendigity.maven.plugin.lifecycle.helper;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.lifecycle.DefaultLifecycles;
@@ -16,7 +17,6 @@ import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.utils.logging.MessageUtils;
-import org.codehaus.plexus.util.StringUtils;
 
 import it.serendigity.maven.plugin.lifecycle.helper.vo.MavenExecutionInfo;
 import it.serendigity.maven.plugin.lifecycle.helper.vo.MavenExecutionPlanInfo;
@@ -51,47 +51,48 @@ public abstract class AbstractLifecycleMojo extends AbstractMojo {
 
 	private List<String> pluginToElaborate;
 
-	protected MavenExecutionPlanInfo calculateExecutionPlan(boolean calculateSummary) throws MojoFailureException {
+	protected MavenExecutionPlanInfo calculateExecutionPlan( boolean calculateSummary ) throws MojoFailureException {
 
-		setPluginToElaborate(normalizeParamFilterPlugin());
+		setPluginToElaborate( normalizeParamFilterPlugin() );
 
-		MavenExecutionPlanInfo planInfo = new MavenExecutionPlanInfo(calculateSummary);
+		MavenExecutionPlanInfo planInfo = new MavenExecutionPlanInfo( calculateSummary );
 
 		try {
 
-			MavenExecutionPlan calculateExecutionPlan = lifecycleExecutor.calculateExecutionPlan(session,
-					paramLifecyclePhases);
+			MavenExecutionPlan calculateExecutionPlan = lifecycleExecutor.calculateExecutionPlan( session,
+					paramLifecyclePhases );
 			List<MojoExecution> mojoExecutions = calculateExecutionPlan.getMojoExecutions();
 
 			int order = 0;
 
-			for (MojoExecution mojoExecution : mojoExecutions) {
+			for ( MojoExecution mojoExecution : mojoExecutions ) {
 
-				MavenExecutionInfo info = MavenExecutionInfo.from(order++, mojoExecution, defaultLifecycles);
-				if (validateMavenExecution(info)) {
-					planInfo.addMavenExecutionInfo(info);
+				MavenExecutionInfo info = MavenExecutionInfo.from( order++, mojoExecution, defaultLifecycles );
+				if ( validateMavenExecution( info ) ) {
+					planInfo.addMavenExecutionInfo( info );
 				}
 			}
 
-			if (planInfo.getMavenExecutionsInfo() == null || planInfo.getMavenExecutionsInfo().isEmpty()) {
-				getLog().info("No execution plan item found for requested parameters.");
+			if ( planInfo.getMavenExecutionsInfo() == null || planInfo.getMavenExecutionsInfo().isEmpty() ) {
+				getLog().info( "No execution plan item found for requested parameters." );
 			}
 
-		} catch (Exception e) {
-			throw new MojoFailureException("Cannot calculate Maven execution plan, caused by: " + e.getMessage(), e);
+		}
+		catch (Exception e) {
+			throw new MojoFailureException( "Cannot calculate Maven execution plan, caused by: " + e.getMessage(), e );
 		}
 		return planInfo;
 	}
 
-	protected boolean validateMavenExecution(MavenExecutionInfo info) {
+	protected boolean validateMavenExecution( MavenExecutionInfo info ) {
 		boolean result = true;
 
-		getLog().debug("Validate maven execution ...");
-		result = validatePlugin(info, getPluginsToElaborate());
+		getLog().debug( "Validate maven execution ..." );
+		result = validatePlugin( info, getPluginsToElaborate() );
 
-		if (getLog().isDebugEnabled()) {
+		if ( getLog().isDebugEnabled() ) {
 
-			getLog().debug("Maven execution " + info + ". Validate: " + result);
+			getLog().debug( "Maven execution " + info + ". Validate: " + result );
 		}
 
 		return result;
@@ -106,16 +107,16 @@ public abstract class AbstractLifecycleMojo extends AbstractMojo {
 
 		List<String> result = null;
 
-		if (getParamFilterPlugins() != null) {
+		if ( getParamFilterPlugins() != null ) {
 
 			// Safe null value
-			for (String f : getParamFilterPlugins()) {
+			for ( String f : getParamFilterPlugins() ) {
 
-				if (result == null) {
+				if ( result == null ) {
 					result = new ArrayList<>();
 				}
-				if (f != null) {
-					result.add(f.trim());
+				if ( f != null ) {
+					result.add( f.trim() );
 				}
 			}
 
@@ -125,44 +126,44 @@ public abstract class AbstractLifecycleMojo extends AbstractMojo {
 	}
 
 	/**
-	 * @param info         a {@link MavenExecutionInfo }
+	 * @param info a {@link MavenExecutionInfo }
 	 * @param filterPlugin a list of plugin String (artifact Id)
 	 * @return true if filterPlugin is null or contains the plugin
 	 */
-	protected static boolean validatePlugin(MavenExecutionInfo info, List<String> filterPlugin) {
+	protected static boolean validatePlugin( MavenExecutionInfo info, List<String> filterPlugin ) {
 
 		boolean result = true;
 
 		// Filter plugin
-		if (filterPlugin != null) {
+		if ( filterPlugin != null ) {
 
-			result = filterPlugin.contains(info.getPluginArtifactId());
+			result = filterPlugin.contains( info.getPluginArtifactId() );
 
 		}
 
 		return result;
 	}
 
-	protected void handleOutput(final String output) {
+	protected void handleOutput( final String output ) {
 		String header = headerParametersString();
 
 		String outputWithTitle = header + output;
-		getLog().info(outputWithTitle);
+		getLog().info( outputWithTitle );
 
 	}
 
 	/**
 	 * Mojo parameters header
-	 * 
+	 *
 	 * @return String parameters
 	 */
 	protected String headerParametersString() {
 		String header = MessageUtils.buffer().strong(
-				"\nProject: " + project.getName() + " (" + project.getArtifactId() + ":" + project.getVersion() + ")")
+				"\nProject: " + project.getName() + " (" + project.getArtifactId() + ":" + project.getVersion() + ")" )
 				+ ".";
-		header = header + "\nExecution plan for run phases: " + Arrays.asList(paramLifecyclePhases);
+		header = header + "\nExecution plan for run phases: " + Arrays.asList( paramLifecyclePhases );
 
-		header = header + "\nFilter plugins: " + StringUtils.defaultString(getPluginsToElaborate());
+		header = header + "\nFilter plugins: " + Objects.toString( getPluginsToElaborate(), "" );
 
 		return header;
 	}
@@ -175,7 +176,7 @@ public abstract class AbstractLifecycleMojo extends AbstractMojo {
 		return pluginToElaborate;
 	}
 
-	protected void setPluginToElaborate(List<String> pluginToElaborate) {
+	protected void setPluginToElaborate( List<String> pluginToElaborate ) {
 		this.pluginToElaborate = pluginToElaborate;
 	}
 
